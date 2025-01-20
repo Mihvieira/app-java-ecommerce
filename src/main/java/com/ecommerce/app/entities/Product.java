2d821ad51c15028cb4def39a0adbd2fc8e53ef29
MihVieira <mih.vieira1@gmail.com>
2025-01-20 12:44:17 -0400

    Product-OrderItem one-to-many association

--- src/main/java/com/ecommerce/app/entities/Product.java
+++ src/main/java/com/ecommerce/app/entities/Product.java
@@ -25,6 +25,8 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name="category_id"))
     private Set<Category> categories = new HashSet<>();
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(Long id, String name, String description, Double price, String imgUrl, Category category) {
         this.id = id;
@@ -77,6 +79,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
