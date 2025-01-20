040816425056b26656389ec01ecd1e0ef05d243e
MihVieira <mih.vieira1@gmail.com>
2025-01-20 11:07:58 -0400

    Order, Instant, ISO 8601

--- src/main/java/com/ecommerce/app/entities/Order.java
+++ src/main/java/com/ecommerce/app/entities/Order.java
@@ -1,5 +1,6 @@
 package com.ecommerce.app.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,7 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
     private OrderStatus orderStatus;
     @JsonIgnore
